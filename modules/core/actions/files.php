d9fdd413d4d5c85226e35b9c72777841289bdce5
antevasin-app <software@antevas.in>
2024-08-30 10:32:47 +0100

    files update

--- modules/core/actions/files.php
+++ modules/core/actions/files.php
@@ -25,9 +25,9 @@ switch ( $action )
             $source = $core->get_module_info( $module_path )->source;
             if ( $private )
             {
-                print_rr('need to add key to header for curl request');
+                // print_rr('need to add key to header for curl request');
                 $token = $core->get_module_info( $module_path )->token;
-                print_rr("token is $token");
+                // print_rr("token is $token");
                 $headers = array(
                     'Authorization: token ' . $token
                 );
@@ -45,13 +45,13 @@ switch ( $action )
             curl_setopt( $ch, CURLOPT_FILE, $local_zip_resource );
             curl_setopt( $ch, CURLOPT_USERAGENT, 'Antevasin-App' );
             curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
-            
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );            
             $result = curl_exec($ch);
             if( !$result ) {
-                echo "Error :- " . curl_error( $ch );
+                // handle errors
+                die("Error :- " . curl_error( $ch ));
             }
-            print_rr("curl results is $result");
+            // print_rr("curl result is $result");
         }
         break;
     default:
@@ -109,7 +109,90 @@ switch ( $action )
         $heading = '';
 }
 
-function install( string $source_file, string $dest_dir, string $source ) : bool
+function install_( string $source_file, string $dest_dir, string $source )
+{
+    $fnNoExt = basename( $source_file, ".zip") ;
+    $dest_dir = 'tmp/';
+    $zip = new \ZipArchive;
+    print_rr($zip);
+    // $destination = 'tmp';
+    if ( $zip->open( $source_file, \ZipArchive::CREATE ) === true )
+    {
+        if ( !is_dir( $dest_dir ) )
+        {
+            // mkdir( $dest_dir, 0711 );
+        }
+        $zip->extractTo( $destination );
+        $zip->close();
+    } 
+    else 
+    {
+        return false;
+    }
+}
+
+function install( string $source_file, string $dest_dir, string $source )
+{
+    $zip = new \ZipArchive;
+    // $source_file = 'C:\xampp\htdocs\plugin\tmp\readme.zip';
+    $source_file = 'tmp\readme.zip';
+    // $source_file = 'tmp\test.zip';
+    $dest_dir = 'tmp';
+    // print_rr($source_file);
+    $res = $zip->open( $source_file, \ZipArchive::CREATE );
+    // print_rr($res);
+    if ( $res === true )  
+    {
+        // print_rr($zip);
+        // $zip->renameIndex( 1, '3/testes.txt' );
+        // $zip->addFromString( 'antevasin-app-module-hauora-e0e502feeb0d7ec7a2f2cf8e4223b1f07e81e715/added.txt', 'my added file content goes here' );
+        // print_rr($zip);
+        for ( $i = 0; $i < $zip->numFiles; $i++ ) 
+        {
+            $file_stats = $zip->statIndex( $i );
+            $filename = $zip->getNameIndex($i);
+            $file_info = pathinfo($filename);
+            // print_rr($filename); print_rr($file_info);
+            // $filename = str_replace( 'antevasin-app-module-hauora-e0e502feeb0d7ec7a2f2cf8e4223b1f07e81e715/', '', $file_stats['name'] );
+            // $file_stats['name'] = $filename;
+            // print_rr($file_stats);
+        }
+        if ( file_exists( 'tmp/hauora' ) )
+        {
+            print_rr('file exists');
+            $backup_file_path = 'tmp/hauora_' . time();
+            $backup_filename = basename( $backup_file_path );
+            $backups_dir = 'tmp/backups';
+            rename( 'tmp/hauora', $backup_file_path );
+            if ( !file_exists( $backups_dir ) )
+            {
+                print_rr('ss dir does not exist so create it');
+                mkdir( $backups_dir, 0711 );
+            }
+            print_rr('ss now exists so move folder there');
+            print_rr($backup_file_path); print_rr($backup_filename);
+            rename( $backup_file_path, "$backups_dir/$backup_filename" );            
+        }        
+        $zip->extractTo( $dest_dir );
+        rename( 'tmp/antevasin-app-module-hauora-e0e502feeb0d7ec7a2f2cf8e4223b1f07e81e715', 'tmp/hauora' );
+        // $new_zip = new \ZipArchive;
+        // $new_res = $new_zip->open( $source_file, \ZipArchive::CREATE );
+        // if ( $new_res === TRUE)  
+        // {
+        //     $new_zip->extractTo( $dest_dir );
+        // }
+    } 
+    else 
+    {
+        echo 'failed, code:' . $res;
+    }
+    if ( $zip->close() === false )
+    {
+        print_rr('failed to close zip file');
+    }
+}
+
+function install__( string $source_file, string $dest_dir, string $source ) : bool
 {
     $zip = new \ZipArchive();
     $resource = $zip->open( $source_file );
@@ -118,46 +201,96 @@ function install( string $source_file, string $dest_dir, string $source ) : bool
         printf('Could not open zip file "%s", error: %d', $source_file, $resource);
         return false;
     }    
-    $test = str_replace( '/', '-', $source );
-    print_rr($test);
+    $file_source = str_replace( '/', '-', $source );
+    print_rr("source file is $source_file - destination dir is $dest_dir - source is $source - file source string is $file_source");
     // Read every file from archive
     for ( $i = 0; $i < $zip->numFiles; $i++ ) 
     {
         $file_stats = $zip->statIndex( $i );
-        $filename = basename( $file_stats['name'] );
-        // print_rr($source); print_rr($filename);
-        if ( str_starts_with( $filename, str_replace( '/', '-', $source ) ) )
+        $filename = $file_stats['name'];
+        // print_rr($file_stats);
+        if ( $filename == 'antevasin-app-module-hauora-e0e502feeb0d7ec7a2f2cf8e4223b1f07e81e715/actions/hauora.php' )
         {
-            $top_level_dir = $file_stats['name'];
-            print_rr("top level directory $top_level_dir");
-            continue;
+            $filename = $zip->getNameIndex($i);
+            $file_info = pathinfo($filename);
+            print_rr($filename); print_rr($file_info);
+            $zip->renameName( 'antevasin-app-module-hauora-e0e502feeb0d7ec7a2f2cf8e4223b1f07e81e715/actions/hauora.php', 'actions/hauora.php' );
+            // print_rr("my file $filename - file is $file");
+            // if ( $zip->extractTo( 'tmp', $filename ) === false ) 
+            // {
+            //     print_rr("Could not extract file $file to ");
+            //     continue;
+            // }  
+            
         }
-        $foldername = str_replace( array( '/', '\\' ), DIRECTORY_SEPARATOR, $dest_dir );
-        print_rr("foldername is $foldername - desitcation directory is $dest_dir");
-        // $absoluteFilename = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $dest_dir . $filename);
-        if ( !is_dir( $foldername ) && !mkdir( $foldername, 0711, true ) && !is_dir( $foldername ) ) 
-        {
-            print_rr("Could not create directory $foldername");
-            return false;
-        }
+        // $filename = $file_stats['name'];
+        // $regex = '/antevasin-app-module-hauora-[A-Za-z0-9]*\/$/';
+        // if ( preg_match( $regex, $filename ) )
+        // {
+        //     print_rr("match $filename");
+        //     $zip->extractTo( 'tmp', $filename );
+        // }
+        // print_rr("directory separator is " . DIRECTORY_SEPARATOR);
+        // $file_path = str_replace( $base_dir, $dest_dir . '/', $filename );
+        // // print_rr("file path is $file_path - filename is $filename - base dir is $base_dir");
+        // // $foldername = str_replace( array( '/', '\\' ), DIRECTORY_SEPARATOR, $dest_dir );
+        // if ( preg_match( '/.*\/$/', $file_path ) ) 
+        // {
+        //     // print_rr('is a directory');
+        //     if ( !is_dir( $file_path ) && !mkdir( $file_path, 0711, true ) ) 
+        //     {
+        //         print_rr("Could not create directory $file_path");
+        //         return false;
+        //     }
+        // }
+        // else
+        // {
+        //     print_rr("is a file - file path is $file_path");
+        //     $directory = dirname( $file_path ) . '/';
+        //     $file = basename( $file_path );
+        //     print_rr("directory is $directory - file is $file - filename is $filename");
+        //     if ( $zip->extractTo( $directory, $filename ) === false ) 
+        //     {
+        //         print_rr("Could not extract file $file to $directory");
+        //         continue;
+        //     }        
+        // }
 
-        // Skip if entry is a directory
-        if ( $filename[strlen($filename) - 1] === DIRECTORY_SEPARATOR ) 
-        {
-            print_rr("Skipping directory $filename");
-            continue;
-        }
+        // print_rr($file_stats); print_rr($filename); die('pause');
+        // $zip->extractTo( $foldername, $file_path );
+        // if ( str_starts_with( $filename, str_replace( '/', '-', $source ) ) )
+        // {
+        //     $top_level_dir = $file_stats['name'];
+        //     print_rr("top level directory $top_level_dir");
+        //     continue;
+        // }
+        // $foldername = str_replace( array( '/', '\\' ), DIRECTORY_SEPARATOR, $dest_dir );
+        // print_rr("foldername is $foldername - desitcation directory is $dest_dir");
+        // // $absoluteFilename = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $dest_dir . $filename);
+        // if ( !is_dir( $foldername ) && !mkdir( $foldername, 0711, true ) && !is_dir( $foldername ) ) 
+        // {
+        //     print_rr("Could not create directory $foldername");
+        //     return false;
+        // }
 
-        // Extract file
-        $file_path_ = str_replace( $top_level_dir, '', $file_stats['name'] );
-        $file_path = $file_stats['name'];
-        print_rr("foldername is $foldername - filename is {$file_stats['name'] } - file path is $file_path - adj file path is $file_path_"); 
-        if ( $zip->extractTo( $foldername, $file_path ) === false ) 
-        {
-            print_rr("Could not extract file $file_path to $foldername");
-            continue;
-        }
+        // // Skip if entry is a directory
+        // if ( $filename[strlen($filename) - 1] === DIRECTORY_SEPARATOR ) 
+        // {
+        //     print_rr("Skipping directory $filename");
+        //     continue;
+        // }
+
+        // // Extract file
+        // $file_path_ = str_replace( $top_level_dir, '', $file_stats['name'] );
+        // $file_path = $file_stats['name'];
+        // print_rr("foldername is $foldername - filename is {$file_stats['name'] } - file path is $file_path - adj file path is $file_path_"); 
+        // if ( $zip->extractTo( $foldername, $file_path ) === false ) 
+        // {
+        //     print_rr("Could not extract file $file_path to $foldername");
+        //     continue;
+        // }
     }
+    $zip->extractTo( $dest_dir );
     $zip->close();
     return true;
 }
