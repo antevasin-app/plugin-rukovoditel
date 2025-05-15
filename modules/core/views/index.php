88188ba3a26a215467efc99c68ca08d0b7dee2e5
antevasin-app <software@antevas.in>
2025-05-15 13:34:42 +0100

    Add 'Timezones' update link to core module tabs for easier access

--- modules/core/views/index.php
+++ modules/core/views/index.php
@@ -21,6 +21,11 @@ $core_module_tabs = array(
                         'field_class' => 'plugin-info',
                         'label' => 'Auto Actions',
                         'field' => '<a onclick="plugin.run_module_action( `update_auto_actions` )">Update</a>'
+                    ),
+                    array(
+                        'field_class' => 'plugin-info',
+                        'label' => 'Timezones',
+                        'field' => '<a onclick="plugin.run_module_action( `update_timezones` )">Update</a>'
                     )
                 )
             ),
